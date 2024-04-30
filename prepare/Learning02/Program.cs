44497fa61e8065c4a3ab0cbf0420537fab2db34f
jacebright <jacebright@gmail.com>
2024-04-29 21:04:59 -0500

    Resume and Job class practice

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Intel";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Developer";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jace Bright";
+        myResume._jobs = [job1, job2];
+
+        myResume.Display();
     }
 }
