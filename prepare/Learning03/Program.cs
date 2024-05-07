18d6fa9539bc0951c4d3c72dadf46d3ab99e4958
jacebright <jacebright@gmail.com>
2024-05-06 21:47:23 -0500

    Initial preparation version

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Net.Sockets;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction one = new Fraction();
+        Fraction six = new Fraction(5);
+        Fraction third = new Fraction(3, 4);
+
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+        Console.WriteLine(six.GetFractionString());
+        Console.WriteLine(six.GetDecimalValue());
+        Console.WriteLine(third.GetFractionString());
+        Console.WriteLine(third.GetDecimalValue());
+
+        one.SetBottom(3);
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
     }
 }
