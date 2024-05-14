a9558959c7fd71d1d6e43f6bbfb13f147ac4d662
jacebright <jacebright@gmail.com>
2024-05-13 19:57:06 -0500

    Learning Activity Week 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment mathAssignment = new 
+        MathAssignment("Samuel Bennett", "Multiplication", "7.3", "8-19");
+
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new 
+        WritingAssignment("Mary Waters", "European History", 
+        "The Causes of World War II");
+
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
