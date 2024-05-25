df74a1b3f43e4c6b31ce58bac17d7a3add611437
jacebright <jacebright@gmail.com>
2024-05-25 13:53:10 -0700

    Added comments to explain creativity

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,5 +1,12 @@
 using System;
 
+// Added a spinner to be used when saving and loading a file. Saving and loading
+// files may be nearly instantaneous, but giving the spinner as a visual feedback
+// to the user gives a better user experience.
+// Additionally added code to clear the screen when performing the create a goal
+// saving and loading the file, and recording an event. This is so the screen
+// does not become overwhelming and the user can focus on their current choices.
+
 class Program
 {
     static void Main(string[] args)
